7cd7f03d860117242172d3f9154ad949292a36fd
Samuel <sdomi003@ucr.edu>
2018-09-20 15:12:12 -0700

    sample video

--- aboutMeTab.js
+++ aboutMeTab.js
@@ -1,22 +1,29 @@
-// /*
-// The animation for the aboutme tab.
-//  */
-// var aboutMeAnimation = function( p ) {
-//
-//     var x = 100;
-//     var y = 100;
-//
-//     p.setup = function() {
-//         var canvas = p.createCanvas(p.windowWidth, p.windowHeight/2);
-//         var j = (p.windowHeight / 2);
-//         canvas.position(0, j);
-//     };
-//
-//     p.draw = function() {
-//         p.background('black');
-//         p.fill(255);
-//         p.rect(x,y,50,50);
-//     };
-// };
-//
-// var aboutMe = new p5(aboutMeAnimation, 'aboutMe');
+/*
+The animation for the aboutme tab.
+ */
+
+var aboutMeAnimation = function( p ) {
+    var sampleVideo;
+    var x = 100;
+    var y = 100;
+
+    p.setup = function() {
+        var canvas = p.createCanvas(p.windowWidth, p.windowHeight/2);
+        var j = (p.windowHeight / 2);
+        sampleVideo = p.createVideo(['videoplayback.webm']);
+        sampleVideo.hide();
+    };
+
+    p.draw = function() {
+        p.background(150);
+        // p.fill(255);
+        // p.rect(x,y,50,50);
+        p.image(sampleVideo, 100, 10);
+    };
+
+    p.mousePressed = function() {
+        sampleVideo.play(); // set the video to loop and start playing
+    }
+};
+
+var aboutMe = new p5(aboutMeAnimation, 'aboutMe');
