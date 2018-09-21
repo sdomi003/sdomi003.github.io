d37d2fcd4f39a6dceb31f82ad69b6f80a6266be8
Samuel <sdomi003@ucr.edu>
2018-09-20 20:18:44 -0700

    video plays and canvas can be drawn on concurrently

--- aboutMeTab.js
+++ aboutMeTab.js
@@ -4,26 +4,36 @@ The animation for the aboutme tab.
 
 var aboutMeAnimation = function( p ) {
     var sampleVideo;
-    var x = 100;
-    var y = 100;
+    var canvas;
+    var x;
+    var y;
 
     p.setup = function() {
-        var canvas = p.createCanvas(p.windowWidth, p.windowHeight/2);
-        var j = (p.windowHeight / 2);
-        sampleVideo = p.createVideo(['videoplayback.webm']);
+        x = 500;
+        y = 500;
+
+        sampleVideo = p.createVideo(['second.mp4']);
+        sampleVideo.size(p.windowWidth - 20, 3*p.windowWidth/4);
         sampleVideo.hide();
+
+        canvas = p.createCanvas(p.windowWidth - 20, 3*p.windowWidth/4);
     };
 
     p.draw = function() {
-        p.background(150);
-        // p.fill(255);
-        // p.rect(x,y,50,50);
-        p.image(sampleVideo, 100, 10);
+        p.clear();
+        p.image(sampleVideo, 0, 0);
+
+        p.stroke('black');
+        p.rect(x, y, 50, 50);
+        x += 1;
+
+
     };
 
     p.mousePressed = function() {
         sampleVideo.play(); // set the video to loop and start playing
     }
+
 };
 
 var aboutMe = new p5(aboutMeAnimation, 'aboutMe');
